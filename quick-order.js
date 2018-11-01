947facbac8009bba63d112779e4c9627ddee85f9
Daniel Voicu <daniel.voicu@brandfusion.ro>
2018-11-01 18:43:36 +0200

    update

--- quick-order.js
+++ quick-order.js
@@ -68,6 +68,13 @@ var ajaxDataset = [
   }
 ]
 
+function GetValidDataset(){
+  var renderedLinesItemNos = [];
+  document.querySelectorAll(".itemNoContainer input").forEach(function(el){
+    renderedLinesItemNos.push(el.value);
+  });  
+  return dataset.filter(function(o){return renderedLinesItemNos.includes(o.itemNo)},[]);
+}
 function renderDataset(arr, addNewLine) {
   document.querySelector(".quickOrder-list").innerHTML = "";
   if (addNewLine === false) {    
@@ -93,8 +100,14 @@ function renderDataset(arr, addNewLine) {
         // if (isInAjaxDataset)
         // console.log(e.currentTarget.closest(".quickOrder-row").index());      
         renderSuggestions(e);
-      }
+        renderTotal();
+      } 
     });
+    // el.addEventListener("focusout", function(e){ 
+    //     if(document.querySelector("#suggestionBox") !== null) {
+    //       document.querySelector("#suggestionBox").remove();
+    //     }
+    // });
   });
   // document.querySelectorAll(".quickOrder-list .itemNoContainer input").forEach(function(el) {
   //   el.addEventListener("focusin", function(e){      
@@ -113,22 +126,19 @@ function renderDataset(arr, addNewLine) {
         //verifies if itemNo is invalid on line
         var itemNo = e.currentTarget.closest(".quickOrder-row").querySelector(".itemNoContainer input").value;        
         if (dataset.filter(function(o){return o.itemNo === itemNo}).length > 0) {
-          var value = parseFloat(e.currentTarget.value);
-          //updates dataset
-          dataset = [].concat(dataset.reduce(function(r,v,i){
-            if (v.itemNo === itemNo) {
-              v.qty = value;
-            }
-            r.push(v);
-            return r;
-          },[]));  
-          var shouldRerenderWithNewLine = inputCollection.indexOf(e.currentTarget) !== inputCollection.length -1;                    
-          renderDataset(dataset,shouldRerenderWithNewLine);
-          //focus on itemNo of new line
-          if (shouldRerenderWithNewLine) {
-            inputCollection[inputCollection.length-2].focus();
-          }
-          
+          // var value = parseFloat(e.currentTarget.value);
+          // //updates dataset
+          // dataset = [].concat(dataset.reduce(function(r,v,i){
+          //   if (v.itemNo === itemNo) {
+          //     v.qty = value;
+          //   }
+          //   r.push(v);
+          //   return r;
+          // },[]));  
+          // var shouldRerenderWithNewLine = inputCollection.indexOf(e.currentTarget) !== inputCollection.length -1; 
+          var indexOfNextInputToBeFocused = inputCollection.indexOf(e.currentTarget) + 1;                   
+          // renderDataset(dataset,false);          
+          inputCollection[indexOfNextInputToBeFocused].focus(); 
         }
     });
   });
@@ -159,10 +169,10 @@ function renderQuickOrderLine(o) {
     output += '<div class="row">';
     output += '<div class="col-xs-2 itemNoContainer"><input type="text" class="form-control" value="'+ o.itemNo +'"/></div>';                  
     output += '<div class="col-xs-1 quantityContainer"><input type="text" class="form-control" value="'+ o.qty +'"/></div>';
-    output += '<div class="col-xs-4"><a href="'+o.link+'">'+o.description+'</a></div>';
-    output += '<div class="col-xs-2">'+ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(o.unitPrice)+'</div>';
-    output += '<div class="col-xs-2">'+ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(o.unitPrice*o.qty)+'</div>';
-    output += '<div class="col-xs-1"><i class="fa fa-trash-o" aria-hidden="true"></i></div>';
+    output += '<div class="col-xs-4 description"><a href="'+o.link+'">'+o.description+'</a></div>';
+    output += '<div class="col-xs-2 unitPrice">'+ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(o.unitPrice)+'</div>';
+    output += '<div class="col-xs-2 lineTotal">'+ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(o.unitPrice*o.qty)+'</div>';
+    output += '<div class="col-xs-1 delete"><i class="fa fa-trash-o" aria-hidden="true"></i></div>';
     output += '</div>';
     output += '</div>';  
   } else {    
@@ -170,10 +180,10 @@ function renderQuickOrderLine(o) {
     output += '<div class="row">';
     output += '<div class="col-xs-2 itemNoContainer"><input type="text" class="form-control" value=""/></div>';                  
     output += '<div class="col-xs-1 quantityContainer"><input type="text" class="form-control" value="1"/></div>';
-    output += '<div class="col-xs-4"></div>';
-    output += '<div class="col-xs-2"></div>';
-    output += '<div class="col-xs-2"></div>';
-    output += '<div class="col-xs-1"></div>';
+    output += '<div class="col-xs-4 description"></div>';
+    output += '<div class="col-xs-2 unitPrice"></div>';
+    output += '<div class="col-xs-2 lineTotal"></div>';
+    output += '<div class="col-xs-1 delete"></div>';
     output += '</div>';
     output += '</div>';  
   }
@@ -181,11 +191,22 @@ function renderQuickOrderLine(o) {
   return output;
 }
 
+function getNodeIndex(node) {
+  var index = 0;
+  while ( (node = node.previousSibling) ) {
+      if (node.nodeType != 3 || !/^\s*$/.test(node.data)) {
+          index++;
+      }
+  }
+  return index;
+}
+
 function renderTotal() {
   if(document.querySelector("#total") !== null) {
     document.querySelector("#total").remove();
   }
-  var total = dataset.reduce(function(r,v,k){
+  var validDataset = GetValidDataset();
+  var total = validDataset.reduce(function(r,v,k){
     r = r + v.qty*v.unitPrice;
     return r;
   },0);
@@ -197,10 +218,14 @@ function renderTotal() {
 }
 
 function renderSuggestionBox(data, val) {  
+  var alreadyFilledItemNumbers = [];
+  document.querySelectorAll(".itemNoContainer input").forEach(function(el){ 
+    alreadyFilledItemNumbers.push(el.value);  
+  });     
   var output = "";
   output += '<div id="suggestionBox">';
   data.map(function(o){ 
-    var isAlreadyInListClass = dataset.filter(function(o2) {return o2.itemNo === o.itemNo}).length > 0 ? "disabled" : "";    
+    var isAlreadyInListClass = alreadyFilledItemNumbers.filter(function(val) {return val === o.itemNo}).length > 0 ? "disabled" : "";    
     output+= '<div class="suggestionLine '+ isAlreadyInListClass +'" data-itemNo="'+o.itemNo+'">'+o.itemNo+' - '+ o.description+' </div>'
   });
   output += '</div>';
@@ -211,10 +236,18 @@ function registerEventsSuggestionBox() {
   document.querySelectorAll(".suggestionLine").forEach(function(el){
     el.addEventListener("click", function(e){
       var itemNo = e.currentTarget.attributes["data-itemNo"].value;
-      if(!e.currentTarget.classList.contains("disabled")) {
-        dataset = dataset.concat(ajaxDataset.filter(function(o){return o.itemNo === itemNo}));
-        renderDataset(dataset, false);
-        inputCollection[inputCollection.length-1].focus();
+      if(!e.currentTarget.classList.contains("disabled")) {        
+        var indexOfLine = getNodeIndex(e.currentTarget.closest(".quickOrder-row"));                    
+        var indexOfQuantityToBeFocused = inputCollection.indexOf(e.currentTarget.closest(".quickOrder-row").querySelector(".quantityContainer input"));
+        if(dataset[indexOfLine] === undefined) { //edit on new line                   
+          dataset = dataset.concat(ajaxDataset.filter(function(o){return o.itemNo === itemNo})); 
+          renderDataset(dataset, true);
+        } else { //edit on already existing line
+          dataset[indexOfLine] = ajaxDataset.filter(function(o){return o.itemNo === itemNo})[0];    
+          renderDataset(dataset, true);                              
+        }
+        inputCollection[indexOfQuantityToBeFocused].focus();
+        
       }
     });
   });
@@ -222,6 +255,12 @@ function registerEventsSuggestionBox() {
 
 function renderSuggestions(e) {
   var val = e.currentTarget.value;  
+  var nodeRow = e.currentTarget.closest(".quickOrder-row");
+  nodeRow.querySelector(".quantityContainer input").value = 1;
+  nodeRow.querySelector(".description").innerHTML = "";
+  nodeRow.querySelector(".unitPrice").innerHTML = "";
+  nodeRow.querySelector(".lineTotal").innerHTML = "";
+  nodeRow.querySelector(".delete").innerHTML = "";  
   var data = ajaxDataset.filter(function(o) {return o.itemNo.includes(val)});  
   if(document.querySelector("#suggestionBox") !== null) {
     document.querySelector("#suggestionBox").remove();
@@ -234,7 +273,7 @@ function renderSuggestions(e) {
 
 function calculateInputCollection() {
   inputCollection = []; 
-  document.querySelectorAll(".itemNoContainer input").forEach(function(el){ 
+  document.querySelectorAll("input").forEach(function(el){ 
     inputCollection.push(el);  
   });   
 }
@@ -244,7 +283,17 @@ function calculateInputCollection() {
 var inputCollection = [];
 renderDataset(dataset, false);
 
+
+document.querySelector("body").addEventListener("keyup", function(e){
+  if (e.keyCode ===27) {
+    if(document.querySelector("#suggestionBox") !== null) {
+      document.querySelector("#suggestionBox").remove();
+    }
+  }
+})
+
 document.querySelector(".quickOrder-list").addEventListener("keydown", function(e){  
+  var i = inputCollection.indexOf(e.target); 
   if (e.keyCode === 9) {
     e.preventDefault();    
     if (e.target.value === "" || (e.target.parentNode.classList.contains("itemNoContainer") && dataset.filter(function(o){return o.itemNo === e.target.value}).length === 0)) {
@@ -260,15 +309,24 @@ document.querySelector(".quickOrder-list").addEventListener("keydown", function(
     } else {
       e.target.parentNode.classList.remove("has-error");
     }
-
-
-    var i = inputCollection.indexOf(e.target); 
-    if(i < inputCollection.length-1) {      
-      inputCollection[i+1].focus();      
+   
+    var itemNo = e.target.closest(".quickOrder-row").querySelector(".itemNoContainer input").value;      
+    var lineIsValid = dataset.filter(function(o){return o.itemNo === itemNo}).length > 0;
+    if(!e.target.parentNode.classList.contains("itemNoContainer")) {            
+      if (lineIsValid) {       
+        inputCollection[i+1].focus();            
+      } else {
+        new Noty({
+          timeout: "2000",
+          theme: 'relax',
+          type: 'error',
+          layout: 'center',
+          text: 'Error appeared on the line. Item number is invalid !'
+        }).show();
+      } 
+      
     } else {  
-      //verifies if itemNo is invalid on line
-      var itemNo = e.target.closest(".quickOrder-row").querySelector(".itemNoContainer input").value;  
-      var lineIsValid = dataset.filter(function(o){return o.itemNo === itemNo}).length > 0;
+      //verifies if itemNo is invalid on line  
       if (lineIsValid) {
         renderDataset(dataset,true);     
         inputCollection[i+1].focus();
@@ -280,8 +338,14 @@ document.querySelector(".quickOrder-list").addEventListener("keydown", function(
           layout: 'center',
           text: 'Error appeared on the line. Item number is invalid !'
         }).show();
-      }
-        
+      }        
     }
-  }  
+  } 
+});
+
+document.querySelector("#addAllLinesToCart").addEventListener("click", function(e){
+  e.preventDefault();
+  var validDataset = GetValidDataset(); //filters out invalid/incomplete itemNumbers  
+  var itemsAddedToCart = validDataset.reduce(function(r,v,k){return r + " " +v.itemNo + " qty:" + v.qty + " | "},"");
+  alert(itemsAddedToCart);
 });
